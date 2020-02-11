a1e36706ec9ceea9a71e386321a43d01bf647acd
Lee Dong Wook <sh95119@gmail.com>
2020-02-12 06:46:04 +0900

    Revert "Update ProductAdd.js"
    This reverts commit e9da5c253e4623b867efef454c67f4599576e1b2.

--- client/src/Components/ProductAdd.js
+++ client/src/Components/ProductAdd.js
@@ -357,7 +357,6 @@ class CustomerAdd extends React.Component {
                                                 value={this.state.fileName}
                                                 onChnage={this.handleFileChange}
                                             />
-                                            // ㅅㄷㄴㅅ
                                             <label htmlFor="contained-button-file">
                                                 <Button className={classes.icon_button}
                                                         variant="contained"
