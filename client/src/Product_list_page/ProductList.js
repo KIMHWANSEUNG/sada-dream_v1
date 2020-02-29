8c381a686d9fa04ea83977f9a36efb5eeadeb86c
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2020-02-29 18:56:24 +0900

    Add: 카드뷰와 카드뷰 안에 컴포넌트 추가
    Add: 카드뷰와 카드뷰 안에 컴포넌트 추가

--- client/src/Product_list_page/ProductList.js
+++ client/src/Product_list_page/ProductList.js
@@ -5,8 +5,6 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Hidden from '@material-ui/core/Hidden';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import Content from './Content';
-import Navigator from './Navigator';
 import { Divider, Grid } from '@material-ui/core';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import List from '@material-ui/core/List';
@@ -14,7 +12,11 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import SendIcon from '@material-ui/icons/Send';
-
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import {Button } from '@material-ui/core';
+import CardActionArea from '@material-ui/core/CardActionArea'
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -27,12 +29,6 @@ function Copyright() {
     </Typography>
   );
 }
-
-
-
-
-
-
 
 const styles = {
   div_root: {
@@ -71,7 +67,8 @@ const styles = {
   Grid_content:{
       marginTop:15,
       marginLeft:30,
-      width:1200 //컨텐츠 그리드 길이
+      width:1300, //컨텐츠 그리드 길이
+
   },
   ListItemText_Category:{
     width:150,
@@ -87,6 +84,41 @@ const styles = {
   },
   Typography_content:{
     marginTop:15
+  },Button_Showlist:{
+    marginLeft:20
+  },Card_Content:{
+    width:500,
+    height:230,
+    marginLeft:50,
+    marginTop:20
+  },Grid_cardmedia:{
+    width:250,
+    height:230
+  },
+  CardMedia_photo:{
+    height:230
+  },
+  Typography_request_country:{
+    fontSize:18
+  },
+  Typography_product_name:{
+    fontSize:25,
+    paddingBottom:5
+  },
+  Typography_product_explain:{
+    fontSize:20,
+    paddingBottom:10
+
+  },
+  Typography_product_cost:{
+    fontSize:25
+  },
+  Typography_after_time:{
+    fontSize:15,
+    paddingTop:15
+  },
+  Grid_content_product:{
+    width:250
   }
   
   
@@ -96,7 +128,12 @@ class ProductList extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-          product_number:100
+          product_number:100,
+          request_country:'일본',
+          product_name:"루이비통 가방",
+          product_explain:"파리 매장에서 팔아요~",
+          product_cost:3000,
+          after_time:1,
 
         }
     }
@@ -189,20 +226,72 @@ render(){
                 </List>
               </Grid>
 
+            
+              <Grid container direction="column" justify="flex-start" alignItems="flex-start" className={classes.Grid_content}>
+                
+                {/* 상품 건수, 목록 버튼 */}
+                <Grid container  direction="row" justify="flex-start"  alignItems="center"  className={classes.Grid_content}>
+                <Typography className={classes.Typography_content} variant="h5" >{this.state.product_number}</Typography> <Typography className={classes.Typography_content} variant="h5">건</Typography>
+                  <Button className={classes.Button_Showlist}>최신순</Button>
+                  <Button className={classes.Button_Showlist}>마감순</Button>
+                  <Button className={classes.Button_Showlist}>가격낮은순</Button>
+                  <Divider></Divider>
+                </Grid>
+              
               {/* 컨텐츠(상품카드) */}
-              <Grid  container  direction="row" justify="flex-start"  alignItems="center"  className={classes.Grid_content}>
-              <Typography className={classes.Typography_content} variant="h5" >{this.state.product_number}</Typography> <Typography className={classes.Typography_content} variant="h5">건</Typography>
-           
-                  
-              
-              </Grid>
-              
-              <Grid  container  direction="row" justify="flex-start"  alignItems="center"  className={classes.Grid_content}>
-                  
-             
-              </Grid>
+              <Grid container> 
 
-            </Grid>
+              <Card className={classes.Card_Content}>
+                <Grid container direction="row" justify="flex-start" alignItems="flex-start">
+                    {/* 이미지 그리드 */}
+                    <Grid className={classes.Grid_cardmedia}  >
+                    <CardActionArea>
+                   
+                    <CardMedia
+                      className={classes.CardMedia_photo}
+                      image="./cookie.jpg"
+                      title="Contemplative Reptile"
+                      />
+                    </CardActionArea>
+                    </Grid>
+            
+                    {/* 내용 그리드 */}
+                    <Grid
+                      className={classes.Grid_content_product}
+                      container2
+                      direction="row"
+                      justify="flex-start"
+                      alignItems="flex-start">
+                      <CardActionArea>
+                          <Typography className={classes.Typography_request_country}>한국에서</Typography>
+                          <Typography className={classes.Typography_request_country}>{this.state.request_country}으로 구매요청</Typography>
+                          <Divider></Divider>
+                          <Typography className={classes.Typography_product_name}>{this.state.product_name}</Typography>
+                          <Typography className={classes.Typography_product_explain}>{this.state.product_explain}</Typography>
+                          <Typography className={classes.Typography_product_cost}>{this.state.product_cost}원</Typography>
+                          <Typography className={classes.Typography_after_time}>{this.state.after_time}시간 전</Typography>
+                          
+
+                       </CardActionArea>
+                    </Grid>
+                </Grid>
+              </Card>
+
+              
+              
+                
+              
+               
+          </Grid>
+
+              
+        </Grid>   
+            
+
+             
+             
+
+      </Grid>
 
 
           
