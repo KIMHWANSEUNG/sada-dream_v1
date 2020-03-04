bd82cfbb622a4bff5a203aa6c364d6a9b8750697
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2020-03-04 21:14:46 +0900

    Add: position:'absolute' 추가
    position:'absolute'

--- client/src/Product_list_page/ProductList.js
+++ client/src/Product_list_page/ProductList.js
@@ -18,7 +18,8 @@ import CardContent from '@material-ui/core/CardContent';
 import {Button } from '@material-ui/core';
 import CardActionArea from '@material-ui/core/CardActionArea'
 import foo from "./cookie.jpg";
-
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
@@ -32,18 +33,19 @@ function Copyright() {
   );
 }
 
-const styles = {
+const styles = theme =>({
   div_root: {
     display: 'flex',
     minHeight: '100vh',
     paddingRight:200,
-    paddingLeft:200
+    paddingLeft:200,
+
   },
  div_app: {
     flex: 1,
     display: 'flex',
     flexDirection: 'column',
-
+ 
   },
   main: {
     flex: 1,
@@ -66,11 +68,13 @@ const styles = {
       width:150,
       minHeight:500
   },
-  Grid_content:{
-      marginTop:15,
-      marginLeft:30,
-      width:1300, //컨텐츠 그리드 길이
+  Grid_content_header:{
 
+  },
+  Grid_content:{
+      marginLeft:200,
+      width:1300, //컨텐츠 그리드 길이
+      position:'absolute' //화면확대 해도 고정시키는 기능 !!
   },
   ListItemText_Category:{
     width:150,
@@ -86,14 +90,17 @@ const styles = {
   },
   Typography_content:{
     marginTop:15
-  },Button_Showlist:{
+  },
+  Button_Showlist:{
     marginLeft:20
-  },Card_Content:{
+  },
+  Card_Content:{
     width:500,
-    height:230,
+    height:210,
     marginLeft:50,
     marginTop:20
-  },Grid_cardmedia:{
+  },
+  Grid_cardmedia:{
     width:250,
     height:230
   },
@@ -101,7 +108,7 @@ const styles = {
     height:230
   },
   Typography_request_country:{
-    fontSize:18
+    fontSize:13
   },
   Typography_product_name:{
     fontSize:25,
@@ -121,10 +128,47 @@ const styles = {
   },
   Grid_content_product:{
     width:250
-  }
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: (theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: (theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
   
   
-};
+});
 
 class ProductList extends React.Component{
     constructor(props){
@@ -232,17 +276,30 @@ render(){
               <Grid container direction="column" justify="flex-start" alignItems="flex-start" className={classes.Grid_content}>
                 
                 {/* 상품 건수, 목록 버튼 */}
-                <Grid container  direction="row" justify="flex-start"  alignItems="center"  className={classes.Grid_content}>
+                <Grid container  direction="row" justify="flex-start"  alignItems="center"  className={classes.Grid_content_header}>
                 <Typography className={classes.Typography_content} variant="h5" >{this.state.product_number}</Typography> <Typography className={classes.Typography_content} variant="h5">건</Typography>
                   <Button className={classes.Button_Showlist}>최신순</Button>
                   <Button className={classes.Button_Showlist}>마감순</Button>
                   <Button className={classes.Button_Showlist}>가격낮은순</Button>
+                  <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
                   <Divider></Divider>
                 </Grid>
               
-              {/* 컨텐츠(상품카드) */}
               <Grid container> 
-
+              
+              {/* 컨텐츠(상품카드) */}
               <Card className={classes.Card_Content}>
                 <Grid container direction="row" justify="flex-start" alignItems="flex-start">
                     {/* 이미지 그리드 */}
@@ -276,9 +333,11 @@ render(){
 
                        </CardActionArea>
                     </Grid>
-                </Grid>
-              </Card>
+                  </Grid>
+                </Card>
 
+                
+ 
           </Grid>
 
         </Grid>   
