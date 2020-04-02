09917dea217ee489628b549d503927afadefa149
Lee Dong Wook <sh95119@gmail.com>
2020-04-03 01:24:07 +0900

    ADD : 리액트 라우터 설치
    리액트 라우터 및 링크 생성

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 // 상품 등록 페이지
-//import ProductAdd from './Product_add_page/ProductAdd';
+import ProductAdd from './Product_add_page/ProductAdd';
 
 //상품 목록 페이지
-//import ProductList from './Product_list_page/ProductList' 
+import ProductList from './Product_list_page/ProductList' 
 
 //회원가입 페이지
 import Register from './Register/Register'
@@ -22,7 +23,11 @@ function App() {
   });
   return (
     <MuiThemeProvider theme={theme}>
-      <Register/>
+      <Switch>
+        <Route exact path='/product/add' component={ProductAdd} />
+        <Route exact path='/product/list' component={ProductList} />
+        <Route exact path='/user/register' component={Register} />
+      </Switch>
     </MuiThemeProvider>
   );
 }
