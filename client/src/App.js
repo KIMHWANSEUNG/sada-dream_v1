d7aa744126cb45bd51b79ded6e2431b93af468e7
KIMHWANSEUNG <49117761+KIMHWANSEUNG@users.noreply.github.com>
2020-04-26 15:52:54 +0900

    good
    good

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,26 @@ import Traveler from "./components/trip/Traveler.js";
 import Travelerlist from "./components/trip/Travelerlist.js";
 import Travelerdetail from "./components/trip/Travelerdetail.js";
 
+import ProductAdd from './Product_add_page/ProductAdd';
+
+//상품 목록 페이지
+import ProductList from './Product_list_page/ProductList' 
+
+//회원가입 페이지
+import Register from './Register/Register'
+
+//로그인 페이지
+import Login from './Login/Login'
+
+//FAQ 페이지
+import FAQ from './FAQ/FAQ'
+
+//고객센터
+import ServiceCenter from './Service center/ServiceCenter'
+
+//문의작성 
+import QuestionAdd from './QuestionAdd/QuestionAdd'
+
 export default class App extends React.Component {
 
   render() {
@@ -26,6 +46,13 @@ export default class App extends React.Component {
             <Route exact path={"/Traveler"} component={Traveler}></Route>
             <Route exact path={"/Travelerlist"} component={Travelerlist}></Route>
             <Route exact path={"/Travelerdetail"} component={Travelerdetail}></Route>
+            <Route exact path='/product/add' component={ProductAdd} />
+            <Route exact path='/product/list' component={ProductList} />
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/faq' component={FAQ}/>
+            <Route exact path='/servicecenter' component={ServiceCenter}/>
+            <Route exact path='/quetionadd' component={QuestionAdd}/>
           </Switch>
           <Footer />
         </div>
