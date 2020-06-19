d9c5fa364c1a72110c1fee7e4a0722fe2a1c3885
JAESUNG PARK <54814562+Jackson-park@users.noreply.github.com>
2020-06-19 15:52:13 +0900

    체킹

--- client/src/components/Appbar/LinkAppbar.js
+++ client/src/components/Appbar/LinkAppbar.js
@@ -16,18 +16,14 @@ const styles = theme =>({
     fontFamily: "Jalnan",
   },
   Link_menu: {
-    fontSize: 14,
+    fontSize: 18,
     fontFamily: "Jalnan",
-    '&:focus, &:hover, &$active': {
+    margin: theme.spacing(2),
+    '&:focus, &:hover': {
       color: 'orange',
     },
   },
-  
-  div_margin: {
-	'& > * + *': {
-    marginLeft: theme.spacing(5),
-  },
-  },
+
   div_center: {
     display: 'flex',
     justifyContent: "center",
@@ -35,6 +31,13 @@ const styles = theme =>({
   div_right: {
 		display: 'flex',
     justifyContent: 'flex-end',
+  },
+  Link_loginmenu: {
+    margin: theme.spacing(2),
+
+    '&:focus, &:hover': {
+      color: 'orange',
+    },
   },
 })
 class LinkAppbar extends React.Component {
@@ -62,7 +65,7 @@ class LinkAppbar extends React.Component {
   handle_Popup(){
     var url = "/login"
     var name = "popup test";
-    var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+    var option = "width = 400, height = 600, top = 100, left = 200, location = no"
     window.open(url, name, option);
   }
   handleFormLogout = async (e) => {
@@ -71,13 +74,16 @@ class LinkAppbar extends React.Component {
     
 }
   onLogoutClick = (e) => {
-    e.preventDefault();
     const url = 'users/logout';
 
     axios.get(url, {
         
     }).then(function(response) {
         console.log(response);
+        if(response.data.result == "ok") {
+          window.alert(response.data.message);
+        }
+        // 로그인이 안된 상태에서는 fail 처리 필요
     }).catch(function(error) {
         console.log(error);
     })
@@ -97,7 +103,6 @@ class LinkAppbar extends React.Component {
           justify="space-around"
           alignItems="center">
         <div>
-          
           <Link
             variant="h6"
             underline="none"
@@ -107,19 +112,29 @@ class LinkAppbar extends React.Component {
             {'사다드림'}
           </Link>
           </div>
-          <div className={classes.div_margin}>
+          <div>
+          <Grid container direction="row" justify="space-between" alignItems="center">
+            <div>
             <Link className={classes.Link_menu} href="/product/list" color="inherit" underline="none">1:1구매요청리스트</Link>
             <Link className={classes.Link_menu} href="/Travelerlist" color="inherit" underline="none">여행자리스트</Link>
             <Link className={classes.Link_menu} href="/product/add"  color="inherit" underline="none">1:1구매요청등록</Link>
             <Link className={classes.Link_menu} href="/Traveler" color="inherit" underline="none">여행자등록</Link>
+            </div>
             
-            <Link href="/login" color="inherit" underline="none">로그인</Link>
-            <Link href="/register" color="inherit" underline="none" > 회원가입</Link>            
+            <div>
+            <Link className={classes.Link_loginmenu} href="/login" color="inherit" underline="none"><b>로그인</b></Link>
+            <Link className={classes.Link_loginmenu} href="/register" color="inherit" underline="none" ><b>회원가입</b></Link>            
+            
             { /*로그인 되었을때 */ }
-            <Link className={classes.Link_menu} onClick={this.handle_Popup} color="inherit" underline="none"><MessageIcon/></Link>
+            <IconButton
+                aria-label="userchat"
+                aria-haspopup="true"
+                onClick={this.handle_Popup}
+                color="inherit">
+            <MessageIcon fontSize="large" />
+            </IconButton>
               <IconButton
                 aria-label="usericon"
-                aria-controls="menu-appbar"
                 aria-haspopup="true"
                 onClick={this.handleMenu}
                 color="primary">
@@ -148,8 +163,10 @@ class LinkAppbar extends React.Component {
                 onClick={this.handleClose}>정산현황</MenuItem>
                 <MenuItem
                 value="logout"
-                onClick={this.handleFormLogout, this.handleClose}>로그아웃</MenuItem>
-              </Menu> 
+                onClick={this.handleFormLogout}>로그아웃</MenuItem>
+              </Menu>
+              </div>
+              </Grid>
               </div>
             </Grid>
       </Toolbar>
